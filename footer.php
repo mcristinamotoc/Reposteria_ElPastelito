e0842d4ccf422c956d7b8bf5b2d6a774bde9c591
Cristina M <mcristinamotoc@gmail.com>
2021-04-11 13:34:27 +0200

    add header, footer and index.php pages

---
+++ footer.php
@@ -0,0 +1,59 @@
+<!-- Footer -->
+<footer id="footer" class="py-2 bg-info text-white">
+    <!-- Grid container -->
+    <div class="container p-2">
+        <!--Grid row-->
+        <div class="row">
+
+            <!--Grid column 1-->
+            <div class="text-white col-lg-3 col-md-12">
+                <h5 class="text-uppercase">Contacto</h5>
+                <p>Calle Aragón 568, Local 3 <br> 08026 - Barcelona</p>
+                <p> 667234489 <br> 937 044 013 </p>
+            </div>
+
+            <!--Grid column 2 -->
+            <div class="text-white col-lg-3 col-md-6 mb-2 mb-md-0">
+                <h5 class="text-uppercase">Sobre Nosotros</h5>
+            </div>
+
+            <!--Grid column 2 -->
+            <div class="text-white col-lg-3 col-md-6 mb-2 mb-md-0">
+                <h5 class="text-uppercase">Sistema de Pago</h5>
+            </div>
+
+            <!--Grid column 3 -->
+            <div class=" text-white col-lg-3 col-md-6 mb-2 mb-md-0">
+                <h5 class="text-uppercase mb-0">Redes Sociales</h5>
+            </div>
+
+        </div><!--EndGrid row-->
+    </div><!-- End Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center text-white ">
+        Copyright &copy; El Pastelito 2021
+    </div>
+</footer>
+
+
+<script src="javascript/loginValidation.js"></script>
+<script src="javascript/searchValidation.js"></script>
+<script src="javascript/registerValidation.js"></script>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js "
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n " crossorigin="anonymous ">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js "
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo " crossorigin="anonymous ">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js "
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6 " crossorigin="anonymous ">
+</script>
+
+</body>
+
+</html>
+
+</body>
