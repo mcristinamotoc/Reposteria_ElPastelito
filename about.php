12fdf5b944320e0ed97a4567de1e9b45070a9480
Cristina M <mcristinamotoc@gmail.com>
2021-04-15 23:02:32 +0200

    create about page

---
+++ about.php
@@ -0,0 +1,34 @@
+<!-- Including the database connection file -->
+<?php include("header.php"); ?>
+
+<!-- About Us information  -->
+<div class="card my-2 text-center ">
+    <div class="card-body mx-3 my-1">
+        <h2 class="card-title display-5 text-info">¡Diseñamos la tarta de tus sueños!</h2>
+            <p class="card-text">Somos un pequeño negocio familiar dedicado a la pastelería artesanal especializados en
+                tartas personalizadas en Barcelona.
+                Desde hace casi 10 años que surgió nuestra idea de plasmar los sueños de las personas en nuestras
+                tartas, cupcakes y galletas a precios asequibles para todos.</p>
+            <p class="card-text">Usamos los mejores ingredientes para nuestras elaboraciones y el sutil sabor
+                tradicional. Honestidad y sencillez es nuestro lema y lo que nos ayuda a destacarnos en el trabajo.
+                Nuestro deseo es expresar los sueños de nuestros clientes en una deliciosa tarta,
+                cupcake o galleta, y uestra meta es hacer muchos sueños realidad.</p>
+            <p class="card-text"> Nuestro servicio es artesanal y personalizado. En El Pastelito realizamos con cada tarta
+                una pequeña obra de arte, llena de sabor y textura. Puedes encargar la tarta personalizada y original
+                que estás buscando para ese día especial y nos ocuparemos de llevarla a tu domicilio.</p>
+                <a href="#" class="btn btn-info">Contacto</a>
+        </div>
+    </div>
+</div>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS and Font Awesome -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js "
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n " crossorigin="anonymous ">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js "
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo " crossorigin="anonymous ">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js "
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6 " crossorigin="anonymous ">
+</script>
+<script src="https://use.fontawesome.com/6234b7c045.js"></script>
