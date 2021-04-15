a1caf37506ddaa2203cb440413d693f9027e934c
Cristina M <mcristinamotoc@gmail.com>
2021-04-15 23:01:12 +0200

    add footer info and icons

--- footer.php
+++ footer.php
@@ -4,44 +4,54 @@
     <div class="container p-2">
         <!--Grid row-->
         <div class="row">
-
-            <!--Grid column 1-->
+            <!--Grid column 1 -> Address and phone contact-->
             <div class="text-white col-lg-3 col-md-12">
                 <h5 class="text-uppercase">Contacto</h5>
-                <p>Calle Aragón 568, Local 3 <br> 08026 - Barcelona</p>
-                <p> 667234489 <br> 937 044 013 </p>
+                <address><i class="fa fa-map-marker" aria-hidden="true">
+                    </i> Calle Aragón 568, Local 3 <br> 08026 - Barcelona</address>
+                <p> <i class="fa fa-phone-square" aria-hidden="true">
+                    </i> 667234489 <br><i class="fa fa-phone-square" aria-hidden="true"></i> 937 044 013 </p>
             </div>
-
-            <!--Grid column 2 -->
+            <!--Grid column 2 -> About Us Information-->
             <div class="text-white col-lg-3 col-md-6 mb-2 mb-md-0">
-                <h5 class="text-uppercase">Sobre Nosotros</h5>
+                <a href="about.php" class="text-white">
+                    <h5 class="text-uppercase">Sobre Nosotros</h5>
+                </a>
             </div>
-
-            <!--Grid column 2 -->
+            <!--Grid column 2-> Payment system information-->
             <div class="text-white col-lg-3 col-md-6 mb-2 mb-md-0">
                 <h5 class="text-uppercase">Sistema de Pago</h5>
+                <div><i class="fa fa-credit-card" aria-hidden="true"></i> Tarjetas credito </div>
+                <i class="fa fa-cc-visa" aria-hidden="true"></i>
+                <i class="fa fa-cc-mastercard" aria-hidden="true"></i>
+                <i class="fa fa-cc-paypal" aria-hidden="true"></i>
             </div>
-
-            <!--Grid column 3 -->
+            <!--Grid column 3 -> Link to Social Media-->
             <div class=" text-white col-lg-3 col-md-6 mb-2 mb-md-0">
                 <h5 class="text-uppercase mb-0">Redes Sociales</h5>
+                <div>
+                    <a href="https://www.facebook.com/elpastelitobcn" class="text-white">
+                        <i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>
+                </div>
+                <div>
+                    <a href="https://www.instagram.com/elpastelitobcn/" class="text-white">
+                        <i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+                </div>
             </div>
-
-        </div><!--EndGrid row-->
+        </div>
+        <!--EndGrid row-->
     </div><!-- End Grid container -->
-
     <!-- Copyright -->
     <div class="text-center text-white ">
         Copyright &copy; El Pastelito 2021
     </div>
 </footer>
 
-
 <script src="javascript/loginValidation.js"></script>
 <script src="javascript/searchValidation.js"></script>
 <script src="javascript/registerValidation.js"></script>
 
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS and Font Awesome -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js "
     integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n " crossorigin="anonymous ">
 </script>
@@ -51,6 +61,7 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js "
     integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6 " crossorigin="anonymous ">
 </script>
+<script src="https://use.fontawesome.com/6234b7c045.js"></script>
 
 </body>
 
