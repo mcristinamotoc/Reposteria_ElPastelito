b555d41a66b0a5efb1dac563ba26a79b30243ad1
Cristina M <mcristinamotoc@gmail.com>
2021-04-17 17:29:28 +0200

    fix about page

--- about.php
+++ about.php
@@ -20,7 +20,7 @@
         </div>
     </div>
 </div>
-
+<?php include("footer.php"); ?>
 <!-- jQuery first, then Popper.js, then Bootstrap JS and Font Awesome -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js "
     integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n " crossorigin="anonymous ">
